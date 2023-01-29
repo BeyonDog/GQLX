96f1a40e83d58b4ef5ad89c83000c09025262a28
YiZhiXiongXiong <111168231+YiZhiXiongXiong@users.noreply.github.com>
2023-01-29 20:51:27 +0800

    添加Save

--- Assets/Scripts/Tool/JsonManager.cs
+++ Assets/Scripts/Tool/JsonManager.cs
@@ -5,19 +5,62 @@
  *  创建时间：2023-01-28 15:52:44
  *  版 本：1.0
  * =======================================================*/
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using UnityEditor;
 
 public class JsonManager : MonoSingleton<JsonManager>
 {
+    private void Start()
+    {
+        Save();
+    }
 
+    public void Save()
+    {
+        PlayerModelInfoRoot playerModelInfoRoot = new PlayerModelInfoRoot();
+        PlayerModelInfo modelInfo = new PlayerModelInfo();
+        PlayerModelInfo modelInfo2 = new PlayerModelInfo();
+        PlayerModelInfo[] modelInfos = new PlayerModelInfo[5];
+        modelInfo.playerAttributeinfo=PlayerModel.Instance.playerPower;
+        modelInfo2.playerAttributeinfo=PlayerModel.Instance.playeragile;
+        
+        // modelInfos[0].playerAttributeinfo = PlayerModel.Instance.playerPower;
+        // modelInfos[1].playerAttributeinfo = PlayerModel.Instance.playeragile;
+        playerModelInfoRoot.playerAttributeinfos.Add(modelInfo);
+        playerModelInfoRoot.playerAttributeinfos.Add(modelInfo2);
+        
+        var vbasePath = Path.Combine(Application.streamingAssetsPath, "Config");
+        DirectoryInfo dir = new DirectoryInfo(vbasePath);
+        if (!dir.Exists)
+            dir.Create(); //创建文件夹
+            
+        var vconfigPath = Path.Combine(vbasePath, "Save.json");
+        FileInfo fileInfo = new FileInfo(vconfigPath);
+        if (fileInfo.Exists)
+            fileInfo.Delete();
 
- /// <summary>
- /// 玩家信息类
- /// </summary>
- public class PlayerInfo
- {
-   
- }
+        StreamWriter writer = fileInfo.CreateText(); //创建文件
+        writer.Write(JsonUtility.ToJson(playerModelInfoRoot));
+        writer.Flush();       
+        writer.Dispose();
+        writer.Close();
+        
+        AssetDatabase.Refresh(); //刷新unity工程，只能在编辑器模式下使用
+    }
 }
+[Serializable]
+public class PlayerModelInfo
+{
+    public BaseGameAttribute playerAttributeinfo=new BaseGameAttribute();
+}
+    
+public class PlayerModelInfoRoot
+{  
+    public List<PlayerModelInfo> playerAttributeinfos=new List<PlayerModelInfo>();
+        
+} 
